6f0cdfce220bb9e0bee4b142fccfcdbe93f1025a
Oliver Eglseder <oliver.eglseder@co-stack.com>
2022-12-19 20:36:42 +0100

    [BUGFIX] Remove strict types declaration from ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @var array $EM_CONF
  * @var string $_EXTKEY
  *
  * @noinspection PhpVariableNamingConventionInspection
+ * @noinspection PhpMissingStrictTypesDeclarationInspection
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'co-stack.com Logs',
